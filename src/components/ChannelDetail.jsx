6b4ab6bcee526372238eb78a1df1a10dc6966e31
Elizabeth Onyenekwe <lizzy@Elizabeths-MacBook-Air.local>
2022-09-16 09:34:24 +0100

    worked on channel detail and learnt how to pass style as props

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchFromApi } from '../utils/fetchFromApi'
+import {Videos, ChannelCard} from './'
+
 
 const ChannelDetail = () => {
+  const {id} = useParams()
+  const [channel, setChannel] = useState(null)
+  const [videos, setVideos] = useState(null)
+
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`)
+      .then((data) => {
+        setChannel(data?.items[0])
+      })
+      fetchFromApi(`search?part=snippet&channelId=${id}&order=date`)
+      .then((data) => {
+        setVideos(data?.items)
+      })
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+   <Box minHeight="95vh">
+     <Box>
+        <div style={{
+          height:'300px',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+        }} />
+        <ChannelCard channelDetail={channel} marginTop="-110px" />
+      </Box>
+      <Box p={2} display="flex">
+      <Box sx={{ mr: { sm: '100px' } }}/>
+        <Videos videos={videos} />
+      </Box>
+   </Box>
   )
 }
 
