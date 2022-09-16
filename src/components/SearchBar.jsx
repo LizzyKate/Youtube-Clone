b0d338b8ddc5cfd9a0bd89a9bdf1b2477250d38d
Elizabeth Onyenekwe <lizzy@Elizabeths-MacBook-Air.local>
2022-09-16 19:04:48 +0100

    completed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,9 +6,20 @@ import { useNavigate } from 'react-router-dom'
 
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`)
+      setSearchTerm('')
+    }
+  }
   return (
-   <Paper component="form" onSubmit={()=>{}} sx={{borderRadius:20, border:'1px solid #e3e3e3', pl:2, boxShadow:'none', mr:{sm:5}}}>
-    <input className='Search-bar' placeholder='Search...' value="" onChange={()=>{}} style={{border:'none'}}/>
+   <Paper component="form" onSubmit={handleSubmit} sx={{borderRadius:20, border:'1px solid #e3e3e3', pl:2, boxShadow:'none', mr:{sm:5}}}>
+    <input className='Search-bar' placeholder='Search...' value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}} style={{border:'none'}}/>
     <IconButton type="submit" sx={{p:'10px', color:'red'}}>
         <Search/>
     </IconButton>
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Box, Typography } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { fetchFromApi } from '../utils/fetchFromApi'
+import {Videos} from '.'
+import { useParams } from 'react-router-dom'
+
+
+
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([])
+  const { searchTerm } = useParams()
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`)
+    .then((data) => {
+      setVideos(data.items)
+  })
+  }, [searchTerm])
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflow:'auto', height:'90vh', flex:2 }}>
+    <Typography variant='h4' fontWeight="bold" mb="2" sx={{color:'#fff'}}> Search results for 
+      <span style={{color:'#F31503', marginLeft:'10px'}}>{searchTerm}</span> videos
+    </Typography>
+    <Videos videos={videos}/>
+  </Box>
   )
 }
 
